cfc899e19a331fea4bee0849e6994d7706a96dc0
Mindy <mindyyounitimes@gmail.com>
2024-11-18 01:29:06 +0800

    making changes in student copy

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jeanseeeee/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jeanseeeee/Desktop/ions_flutter_student_copy"
+export "FLUTTER_APPLICATION_PATH=/Users/jeanseeeee/Desktop/ions_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/jeanseeeee/Desktop/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jeanseeeee/Desktop/ions_flutter_student_copy"
+export "FLUTTER_APPLICATION_PATH=/Users/jeanseeeee/Desktop/ions_flutter"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/jeanseeeee/Desktop/ions_flutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/jeanseeeee/Desktop/ions_flutter/.dart_tool/package_config.json"
